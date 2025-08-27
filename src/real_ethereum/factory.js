dea1992c005a62269230c0660377c8185da9e498
Nir Ellor <nirellorwaizner@gmail.com>
2025-08-27 17:00:54 +0300

    all should work

--- src/real_ethereum/factory.js
+++ src/real_ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  "0x5409A133308f81Ab861ec158F87297B7b3387489" // Replace with your deployed contract address
+  "0x18244e00edbF44F085F2436bb5322aA3441c5595" // Replace with your deployed contract address
 );
 
 export default instance;
